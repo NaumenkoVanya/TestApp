cd097ac964e90e48447089a08a269af69175ae89
Ваня Науменко <ivannaumenko@MacBook-Pro-Vana.local>
2025-04-02 22:43:36 +0300

    start project

---
+++ TestApp/TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Ваня Науменко on 2.04.25.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
